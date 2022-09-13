b9a3741ae726c7f64b69f4af54a20c6aeac0a79a
Sumit Ojha <sumitojha501@gmail.com>
2022-09-13 18:26:10 -0500

    Created vote model and routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const sequelize = require("../../config/connection");
+const { Post, User, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -21,13 +33,23 @@ router.get("/", (req, res) => {
     });
 });
 
-// get single post
 router.get("/:id", (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -48,7 +70,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-//create a post
 router.post("/", (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
@@ -63,7 +84,16 @@ router.post("/", (req, res) => {
     });
 });
 
-//update a post's title
+router.put("/upvote", (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 router.put("/:id", (req, res) => {
   Post.update(
     {
@@ -88,17 +118,6 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
-router.put("/upvote", (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-  })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => res.json(err));
-});
-
-//delete a post
 router.delete("/:id", (req, res) => {
   Post.destroy({
     where: {
